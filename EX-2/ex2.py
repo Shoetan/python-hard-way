c279d351ba9957efc460cc81d80cb65c3edf79ff
Shoetan <emmysoetan@gmail.com>
2026-02-24 03:25:36 +0100

    [chore]: Exercise 2 Comments

---
+++ EX-2/ex2.py
@@ -0,0 +1,9 @@
+#Comments
+
+#This is a comment, it is not executed by the program
+
+print("I could have code like this") #This is also a comment, it is ignored by the program
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# print("This won't run")
+print("This will run")
